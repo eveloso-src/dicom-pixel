449fac439d3c8f015d605c3804a269e58f3d9a76
Veloso, Emiliano <emiliano.veloso@accenture.com>
2019-04-20 22:16:44 -0300

    Initial commit

---
+++ src/main/java/fxml/controller/ToGray.java
@@ -0,0 +1,87 @@
+package fxml.controller;
+
+import java.awt.AWTException;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayDeque;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+
+public class ToGray {
+
+	public static BufferedImage[] convertBufferedImage(BufferedImage img, ArrayDeque<BufferedImage> imagenes)
+			throws AWTException {
+		int width = img.getWidth();
+		int height = img.getHeight();
+
+		int[][] pixel = new int[width][height];
+		byte[][] pixelBW = new byte[width][height];
+
+		Raster raster = img.getData();
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				pixel[i][j] = raster.getSample(i, j, 0);
+				// pixelBW[i][j] = new Double(raster.getSample(i, j, 0) * 0.21 +
+				// raster.getSample(i, j, 1) * 0.71
+				// + raster.getSample(i, j, 2) * 0.07).byteValue();
+			}
+		}
+
+		BufferedImage theImage = new BufferedImage(1000, 1000, BufferedImage.TYPE_BYTE_GRAY);
+		BufferedImage theImage2 = new BufferedImage(1000, 1000, BufferedImage.TYPE_BYTE_GRAY);
+
+		BufferedImage [] imagesArray = new BufferedImage[2];
+		
+		// conversion automatica a grises
+		Graphics2D graphic = theImage.createGraphics();
+		graphic.drawImage(img, 0, 0, null, null);
+		graphic.dispose();
+
+		graphic = theImage2.createGraphics();
+		graphic.drawImage(img, 0, 0, null, null);
+		graphic.dispose();
+
+		
+		// retocar la imagen por bytes
+		int value = 255;
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				value = pixel[i][j] << 16 | pixel[i][j] << 8 | pixel[i][j];
+				if (i > 0) {
+					value = value - (pixel[i - 1][j] << 16 | pixel[i - 1][j] << 8 | pixel[i - 1][j]);
+				}
+				if (i == 3 || j == 3) {
+					value = 255 << 16 | 255 << 8 | 255;
+				}
+				theImage.setRGB(i, j, value);
+			}
+		}
+
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_hh_mm__ss__SSS");
+
+			imagenes.add(theImage);
+
+			if (imagenes.size() == 200) {
+				for (int i = 0; i < imagenes.size(); i++) {
+					String fileOutput = "imgs/out" + sdf.format(new Date()) + ".gif";
+					File outputPhoto = new File(fileOutput);
+					ImageIO.write(imagenes.poll(), "gif", outputPhoto);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+		imagesArray[0] = theImage;
+		imagesArray[1] = theImage2;
+		return imagesArray;
+	}
+
+}
---
+++ src/main/java/fxml/controller/WebCamPreviewController.java
@@ -0,0 +1,301 @@
+package fxml.controller;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.util.ArrayDeque;
+import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.util.jh.JHGrayFilter;
+
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+
+@SuppressWarnings("restriction")
+public class WebCamPreviewController implements Initializable {
+
+	@FXML
+	Button btnStartCamera;
+	@FXML
+	Button btnStopCamera;
+	@FXML
+	Button btnDisposeCamera;
+	@FXML
+	ComboBox<WebCamInfo> cbCameraOptions;
+	@FXML
+	BorderPane bpWebCamPaneHolder;
+	@FXML
+	FlowPane fpBottomPane;
+	@FXML
+	ImageView imgWebCamCapturedImage;
+
+	@FXML
+	ImageView imgWebCamCapturedImage2;
+	@FXML
+	ImageView imgWebCamCapturedImage3;
+
+	private BufferedImage grabbedImage;
+	private BufferedImage bwImage;
+	private BufferedImage bwImage2;
+	
+	private BufferedImage[] arrayImg;
+	// private WebcamPanel selWebCamPanel = null;
+	private Webcam selWebCam = null;
+
+	private Webcam webcamDefault = Webcam.getDefault();
+
+	private boolean stopCamera = false;
+	private ObjectProperty<Image> imageProperty = new SimpleObjectProperty<Image>();
+	private ObjectProperty<Image> imageProperty2 = new SimpleObjectProperty<Image>();
+	private ObjectProperty<Image> imageProperty3 = new SimpleObjectProperty<Image>();
+
+	private String cameraListPromptText = "Seleccion Camara";
+
+	// @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		fpBottomPane.setDisable(true);
+		ObservableList<WebCamInfo> options = FXCollections.observableArrayList();
+		int webCamCounter = 0;
+		for (Webcam webcam : Webcam.getWebcams()) {
+			WebCamInfo webCamInfo = new WebCamInfo();
+			webCamInfo.setWebCamIndex(webCamCounter);
+			webCamInfo.setWebCamName(webcam.getName());
+			webcam.setViewSize(new Dimension(640, 480));
+			options.add(webCamInfo);
+			webCamCounter++;
+		}
+		cbCameraOptions.setItems(options);
+		// cbCameraOptions.getSelectionModel().select(0);
+		// initializeWebCam(webCamIndex);
+		cbCameraOptions.setPromptText(cameraListPromptText);
+		cbCameraOptions.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<WebCamInfo>() {
+
+			// @Override
+			public void changed(ObservableValue<? extends WebCamInfo> arg0, WebCamInfo arg1, WebCamInfo arg2) {
+				if (arg2 != null) {
+
+					System.out.println(
+							"WebCam Index: " + arg2.getWebCamIndex() + ": WebCam Name:" + arg2.getWebCamName());
+					initializeWebCam(arg2.getWebCamIndex());
+				}
+			}
+		});
+		Platform.runLater(new Runnable() {
+
+			// @Override
+			public void run() {
+
+				setImageViewSize();
+
+			}
+		});
+
+	}
+
+	protected void setImageViewSize() {
+
+		System.out.println("640x480");
+		double height = 1000; // bpWebCamPaneHolder.getHeight();
+		double width = 1000; // bpWebCamPaneHolder.getWidth();
+		imgWebCamCapturedImage.setFitHeight(height);
+		imgWebCamCapturedImage.setFitWidth(width);
+		imgWebCamCapturedImage.prefHeight(height);
+		imgWebCamCapturedImage.prefWidth(width);
+		imgWebCamCapturedImage.setPreserveRatio(true);
+
+		imgWebCamCapturedImage2.setFitHeight(height);
+		imgWebCamCapturedImage2.setFitWidth(width);
+		imgWebCamCapturedImage2.prefHeight(height);
+		imgWebCamCapturedImage2.prefWidth(width);
+		imgWebCamCapturedImage2.setPreserveRatio(true);
+
+		imgWebCamCapturedImage3.setFitHeight(height/2);
+		imgWebCamCapturedImage3.setFitWidth(width/2);
+		imgWebCamCapturedImage3.prefHeight(height/2);
+		imgWebCamCapturedImage3.prefWidth(width/2);
+		imgWebCamCapturedImage3.setPreserveRatio(true);
+	}
+
+	protected void initializeWebCam(final int webCamIndex) {
+
+		Task<Void> webCamIntilizer = new Task<Void>() {
+
+			@Override
+			protected Void call() throws Exception {
+
+				if (selWebCam == null) {
+					selWebCam = Webcam.getWebcams().get(webCamIndex);
+					selWebCam.open();
+				} else {
+					closeCamera();
+					selWebCam = Webcam.getWebcams().get(webCamIndex);
+					selWebCam.open();
+
+				}
+				startWebCamStream();
+				return null;
+			}
+
+		};
+
+		new Thread(webCamIntilizer).start();
+		fpBottomPane.setDisable(false);
+		btnStartCamera.setDisable(true);
+	}
+
+	protected void startWebCamStream() {
+
+		stopCamera = false;
+		Task<Void> task = new Task<Void>() {
+
+			@Override
+			protected Void call() throws Exception {
+				// SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_hh_mm_ss_SSS");
+				// ByteArrayOutputStream baos;
+				// InputStream bais;
+				// ToGray tg = new ToGray();
+
+				ArrayDeque<BufferedImage> imagenes = new ArrayDeque<BufferedImage>(1000);
+
+				int nThreads = Runtime.getRuntime().availableProcessors();
+				System.out.println("procesadores: " + nThreads);
+				ExecutorService executor = Executors.newSingleThreadExecutor();
+
+				while (!stopCamera) {
+					try {
+						if ((grabbedImage = webcamDefault.getImage()) != null) {
+							arrayImg = ToGray.convertBufferedImage(grabbedImage, imagenes);
+
+							bwImage = arrayImg[0];
+							bwImage2 = arrayImg[1];
+							
+							// String file = "imgs/img_" + sdf.format(new Date()) + ".jpg";
+							// ImageIO.write(grabbedImage, "jpg", new File(file));
+							// Platform.runLater(
+
+							// executor.execute(
+							// IntStream.range(0, 100).forEach(i -> service.submit(new Task(i)));
+							executor.submit(new Runnable() {
+								// @Override
+								public void run() {
+									final Image mainiamge = SwingFXUtils.toFXImage(grabbedImage, null);
+									final Image mainiamge2 = SwingFXUtils.toFXImage(bwImage, null);
+									final Image mainiamge3 = SwingFXUtils.toFXImage(bwImage2, null);
+									// Color result = mainiamge.getPixelReader().getColor(0, 0);
+									// System.out.println("result: " + result);
+									imageProperty.set(mainiamge);
+									imageProperty2.set(mainiamge2);
+									imageProperty3.set(mainiamge3);
+
+								}
+
+							});
+
+							// );
+
+							// grabbedImage.flush();
+
+						}
+					} catch (Exception e) {
+					} finally {
+
+					}
+
+				}
+
+				return null;
+
+			}
+
+		};
+		Thread th = new Thread(task);
+		th.setDaemon(true);
+		th.start();
+		imgWebCamCapturedImage.imageProperty().bind(imageProperty);
+		imgWebCamCapturedImage2.imageProperty().bind(imageProperty2);
+		imgWebCamCapturedImage3.imageProperty().bind(imageProperty3);
+
+	}
+
+	private void closeCamera() {
+		if (webcamDefault != null) {
+			webcamDefault.close();
+		}
+	}
+
+	public void stopCamera(ActionEvent event) {
+		stopCamera = true;
+		btnStartCamera.setDisable(false);
+		btnStopCamera.setDisable(true);
+	}
+
+	public void startCamera(ActionEvent event) {
+		stopCamera = false;
+		startWebCamStream();
+
+		btnStartCamera.setDisable(true);
+		btnStopCamera.setDisable(false);
+
+	}
+
+	private static final JHGrayFilter GRAY = new JHGrayFilter();
+
+	public BufferedImage transform(BufferedImage image) {
+		return GRAY.filter(image, null);
+	}
+
+	public void disposeCamera(ActionEvent event) {
+		stopCamera = true;
+		closeCamera();
+		Webcam.shutdown();
+		btnStopCamera.setDisable(true);
+		btnStartCamera.setDisable(true);
+	}
+
+	class WebCamInfo {
+		private String webCamName;
+		private int webCamIndex;
+
+		public String getWebCamName() {
+			return webCamName;
+		}
+
+		public void setWebCamName(String webCamName) {
+			this.webCamName = webCamName;
+		}
+
+		public int getWebCamIndex() {
+			return webCamIndex;
+		}
+
+		public void setWebCamIndex(int webCamIndex) {
+			this.webCamIndex = webCamIndex;
+		}
+
+		@Override
+		public String toString() {
+			return webCamName;
+		}
+
+	}
+}
